c80b6ff208624ac37380cd2e464e8d3b6c6ce243
Pedro Travanca <pedrotravanca1@hotmail.com>
2020-12-10 12:59:20 +0000

    Created User and Organization classes

---
+++ src/main/java/org/academiadecodigo/bitjs/whitelotus/powerpeople/model/persistence/Organization.java
@@ -0,0 +1,5 @@
+package org.academiadecodigo.bitjs.whitelotus.powerpeople.model.persistence;
+
+public class Organization {
+    
+}
--- src/main/java/org/academiadecodigo/bitjs/whitelotus/powerpeople/model/persistence/User.java
+++ src/main/java/org/academiadecodigo/bitjs/whitelotus/powerpeople/model/persistence/User.java
@@ -1,4 +1,8 @@
 package org.academiadecodigo.bitjs.whitelotus.powerpeople.model.persistence;
 
 public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+
 }
